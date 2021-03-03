b3e10ef1240464d9c82159cacf52a59b3ed2381c
ion-pop-gln <75470240+ion-pop-gln@users.noreply.github.com>
2021-03-03 18:55:55 +0200

    Update main.cpp

--- divide_et_impera/pb1/main.cpp
+++ divide_et_impera/pb1/main.cpp
@@ -1,52 +1,100 @@
 #include <iostream>
 
 constexpr unsigned MAX_N { 50 };
-constexpr unsigned MAX_M { 50 };
 
-void intersch(double matrix[MAX_N][MAX_M], unsigned p, unsigned k,
-              unsigned sus, unsigned jos)
+typedef struct Index {
+    unsigned x { 0 };
+    unsigned y { 0 };
+} Index;
+
+void intersch_c(int matrix[MAX_N][MAX_N], unsigned p, unsigned k,
+                  unsigned sus, unsigned jos)
 {
     if (sus == jos) {
         std::swap(matrix[sus][p], matrix[sus][k]);
     } else {
         unsigned mijl { (sus + jos) / 2 };
-        intersch(matrix, p, k, sus, mijl);
-        intersch(matrix, p, k, mijl + 1, jos);
+        intersch_c(matrix, p, k, sus, mijl);
+        intersch_c(matrix, p, k, mijl + 1, jos);
     }
+}
+
+void intersch_r(int matrix[MAX_N][MAX_N], unsigned p, unsigned k,
+                  unsigned st, unsigned dr)
+{
+    if (st == dr) {
+        std::swap(matrix[p][st], matrix[k][st]);
+    } else {
+        unsigned mijl { (st + dr) / 2 };
+        intersch_r(matrix, p, k, st, mijl);
+        intersch_r(matrix, p, k, mijl + 1, dr);
+    }
+}
+
+Index index_max(int matrix[MAX_N][MAX_N], unsigned n, unsigned st, unsigned jos)
+{
+    Index max_xy {};
+    int max { matrix[st][jos] };
+
+    for (unsigned r { jos }; r >= 0; r--) {
+        for (unsigned c { st }; c < n; c++) {
+            if (matrix[r][c] > max) {
+                max = matrix[r][c];
+                max_xy.x = c;
+                max_xy.y = r;
+            }
+        }
+
+        if (r == 0)
+            break;
+    }
+
+    return max_xy;
+}
+
+void sort(int matrix[MAX_N][MAX_N], unsigned n, unsigned st, unsigned jos)
+{
+    if (jos == 0)
+        return;
+
+    Index index { index_max(matrix, n, st, jos) };
+
+    intersch_r(matrix, jos, index.y, st, n - 1);
+    intersch_c(matrix, st, index.x, 0, jos);
+
+    sort(matrix, n, st + 1, jos - 1);
+}
+
+bool is_sorted(int matrix[MAX_N][MAX_N], unsigned n)
+{
+    for (unsigned i { 0 }; i < n - 1; i++)
+        if (matrix[n - 1 - i][i] < matrix[n - 2 - i][i + 1])
+            return false;
+    return true;
 }
 
 int main()
 {
-    double matrix[MAX_N][MAX_M] { 0 };
+    int matrix[MAX_N][MAX_N] { 0 };
 
     std::cout << "Enter n: ";
     unsigned n { 0 };
     std::cin >> n;
 
-    std::cout << "Enter m: ";
-    unsigned m { 0 };
-    std::cin >> m;
-
     std::cout << "Enter elements:" << std::endl;
     for (unsigned r { 0 }; r < n; r++)
-        for (unsigned c { 0 }; c < m; c++)
+        for (unsigned c { 0 }; c < n; c++)
             std::cin >> matrix[r][c];
- 
-    std::cout << "Enter p: ";
-    unsigned p { 0 };
-    std::cin >> p;
-    unsigned index_p { p - 1 };
 
-    std::cout << "Enter k: ";
-    unsigned k { 0 };
-    std::cin >> k;
-    unsigned index_k { k - 1 };
-
-    intersch(matrix, index_p, index_k, 0, n-1);
+    sort(matrix, n, 0, n-1);
+    if (!is_sorted(matrix, n)) {
+        std::cout << "Matricea nu a putut fi sortata >:(" << std::endl;
+        return 1;
+    }
 
     std::cout << "Matricea:" << std::endl;
     for (unsigned r { 0 }; r < n; r++) {
-        for (unsigned c { 0 }; c < m; c++)
+        for (unsigned c { 0 }; c < n; c++)
             std::cout << matrix[r][c] << " ";
         std::cout << std::endl;
     }
